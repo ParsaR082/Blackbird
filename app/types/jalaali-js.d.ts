321a39d4486417f84680375e4419c2bb6150290b
tarla hosseinzadeh <tarla.hosseinzadeh@gmail.com>
2025-07-15 01:45:11 +0330

    add shamsi calender to my calender page

--- app/types/jalaali-js.d.ts
+++ app/types/jalaali-js.d.ts
@@ -1,4 +1,8 @@
 declare module 'jalaali-js' {
   export function toJalaali(gy: number, gm: number, gd: number): { jy: number, jm: number, jd: number };
   export function toGregorian(jy: number, jm: number, jd: number): { gy: number, gm: number, gd: number };
+  export function jalaaliMonthLength(jy: number, jm: number): number;
+  export function isLeapJalaaliYear(jy: number): boolean;
+  export function jalaaliToDateObject(jy: number, jm: number, jd: number): Date;
+  export function dateToJalaali(date: Date): { jy: number, jm: number, jd: number };
 } 
