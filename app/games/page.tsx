cb9d2e3deb2d0431ddd9fdc066eb16537fcb01f8
Parsa <prahmani082@gmail.com>
2025-07-22 13:56:56 -0700

    changed game/page

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,11 +1,9 @@
 import { PrismaClient } from '../../generated/prisma'
 import ClientGamesPage from './ClientGamesPage'
 
-async function GamesPage() {
+export default async function GamesPage() {
   const prisma = new PrismaClient()
   const dbGames = await prisma.game.findMany({ orderBy: { createdAt: 'desc' } })
   await prisma.$disconnect()
   return <ClientGamesPage dbGames={dbGames} />
 }
-
-export default GamesPage
